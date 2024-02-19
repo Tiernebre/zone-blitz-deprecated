c9cf96e807fbd42cd11c1e83c8cb6955c1c786d7
Brendan Tierney <tiernebre@gmail.com>
2024-02-19 19:26:49 +0000

    VERY initial dev script

---
+++ bin/dev.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+while true
+do
+  make serve &
+  PID=$!
+  echo "Got PID=$PID"
+  inotifywait -r -e modify src/main/java/com/tiernebre
+  echo "change happened!"
+  killall make java && wait
+done
