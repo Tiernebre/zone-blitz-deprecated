61dba3fcd7b11b8f77db8586e53c46f33dcc432e
Brendan Tierney <tiernebre@gmail.com>
2024-02-19 19:57:01 +0000

    reload is working

--- bin/dev.sh
+++ bin/dev.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
+make install
+
 while true
 do
-  make serve &
-  PID=$!
-  echo "Got PID=$PID"
+  make dev &
   inotifywait -r -e modify src/main/java/com/tiernebre src/main/resources/templates
-  echo "change happened!"
-  killall make java && wait
+  killall make
+  kill $(pgrep -f "java -jar")
 done
