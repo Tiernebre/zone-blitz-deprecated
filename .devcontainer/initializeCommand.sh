579d6a7dc43167e14953df3c5a473a742d0a84d9
Brendan Tierney <tiernebre@gmail.com>
2024-02-25 17:56:51 +0000

    just remove the mkcert check

--- .devcontainer/initializeCommand.sh
+++ .devcontainer/initializeCommand.sh
@@ -13,12 +13,6 @@ if [ -f "${CERT_FILE}" ]; then
   echo "Certification was already created. Skipping certification creation and proceeding with dev container creation."
 else
   echo "Certification does not exist already for dev container. Proceeding to create certifications."
-  mkcert --help
-  if ! command -v mkcert &> /dev/null; then
-    echo "mkcert is required to run the zone blitz dev container. Please install using the instructions at https://github.com/FiloSottile/mkcert?tab=readme-ov-file#installation"
-    exit 1
-  fi
-
   echo "You _may_ need to require sudo access. This is because mkcert will trust the generated certificate for you to avoid browser issues.\nPlease provide the sudo password, ${bold}otherwise you can run mkcert manually outside of this script.${normal}"
 
   mkcert -install
