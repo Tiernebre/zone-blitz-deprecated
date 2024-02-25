9f1a4ce65ede37797dd69f193e1b58d3d436376a
Brendan Tierney <tiernebre@gmail.com>
2024-02-25 10:52:51 +0000

    working mkcert!

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mkcert -install
-mkcert -cert-file .devcontainer/certs/local-cert.pem -key-file .devcontainer/certs/local-key.pem 0.0.0.0
+mkcert -cert-file .devcontainer/certs/localhost.pem -key-file .devcontainer/certs/localhost.pem localhost
