e6659caa1ba48a82f3dd563df449aebf434e6fc6
Brendan Tierney <tiernebre@gmail.com>
2024-02-25 04:37:57 -0700

    Generate and serve devcontainer proxy with a working `localhost` HTTPS SSL certification for HTTPS development (#35)
    Pretty awesome change that I learned a lot from. This helps get
    zoneblitz running in a dev container WITH HTTPS support.
    
    This will allow us to eventually support any third party APIs or
    services that require an HTTPS connection. It also helps ensure that
    we're more consistent with production to help with any SSL issues on the
    proxy in a dev environment.

---
+++ .devcontainer/initializeCommand.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ -n "${CI}" ]; then
+  echo "Certification does not need to be created as this is being ran in a CI environment. Skipping and proceeding with dev container creation."
+  exit 0
+fi
+
+if [ -f .devcontainer/certs/localhost.cert ]; then
+  echo "Certification was already created. Skipping certification creation and proceeding with dev container creation."
+  exit 0
+else
+  echo "Certification does not exist already for dev container. Proceeding to create certifications."
+fi
+
+if ! command -v mkcert &> /dev/null
+then
+    echo "mkcert is required to run the zone blitz dev container. Please install using the instructions at https://github.com/FiloSottile/mkcert?tab=readme-ov-file#installation"
+    exit 1
+fi
+
+echo "You _may_ need to require sudo access. This is because mkcert will trust the generated certificate for you to avoid browser issues. Please provide the sudo password, otherwise you can run mkcert manually outside of this script."
+
+mkcert -install
+mkcert -cert-file .devcontainer/certs/localhost.cert -key-file .devcontainer/certs/localhost.key localhost
