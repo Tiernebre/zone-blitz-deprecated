31ff39b7d96ae534aaa50abff1782dd8f33482f2
Brendan Tierney <tiernebre@gmail.com>
2024-02-25 11:30:11 +0000

    try this

--- .devcontainer/initializeCommand.sh
+++ .devcontainer/initializeCommand.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -z "${CI}" ]; then
+if [ "$CI" == "true" ]; then
   echo "Certification does not need to be created as this is being ran in a CI environment. Skipping and proceeding with dev container creation."
   exit 0
 fi
