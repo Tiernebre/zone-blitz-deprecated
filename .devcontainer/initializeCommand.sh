26e96ef4192920896e38a2f50e39f1a89aa030ac
Brendan Tierney <tiernebre@gmail.com>
2024-02-25 11:37:00 +0000

    more vague ci

--- .devcontainer/initializeCommand.sh
+++ .devcontainer/initializeCommand.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -n "${GITHUB_ENV}" ]; then
+if [ -n "${CI}" ]; then
   echo "Certification does not need to be created as this is being ran in a CI environment. Skipping and proceeding with dev container creation."
   exit 0
 fi
