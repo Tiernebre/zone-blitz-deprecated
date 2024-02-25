698eeea51d4f676cc9493f1f01e3adbffb4225e7
Brendan Tierney <tiernebre@gmail.com>
2024-02-25 11:32:35 +0000

    makybe this iwll work

--- .devcontainer/initializeCommand.sh
+++ .devcontainer/initializeCommand.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ "$CI" == "true" ]; then
+if [ -z "${GITHUB_ENV}" ]; then
   echo "Certification does not need to be created as this is being ran in a CI environment. Skipping and proceeding with dev container creation."
   exit 0
 fi
