4478dc00209554d5d6c8ba2666ffbe14ac2d3166
Brendan Tierney <tiernebre@gmail.com>
2024-02-25 17:48:22 +0000

    try this

--- .devcontainer/generateCertificates.sh
+++ .devcontainer/generateCertificates.sh
@@ -15,10 +15,9 @@ else
   echo "Certification does not exist already for dev container. Proceeding to create certifications."
 fi
 
-if ! command -v mkcert &> /dev/null
-then
-    echo "mkcert is required to run the zone blitz dev container. Please install using the instructions at https://github.com/FiloSottile/mkcert?tab=readme-ov-file#installation"
-    exit 1
+if [ -n "${CI}" ] && ! command -v mkcert &> /dev/null; then
+  echo "mkcert is required to run the zone blitz dev container. Please install using the instructions at https://github.com/FiloSottile/mkcert?tab=readme-ov-file#installation"
+  exit 1
 fi
 
 echo "You _may_ need to require sudo access. This is because mkcert will trust the generated certificate for you to avoid browser issues.\nPlease provide the sudo password, ${bold}otherwise you can run mkcert manually outside of this script.${normal}"
