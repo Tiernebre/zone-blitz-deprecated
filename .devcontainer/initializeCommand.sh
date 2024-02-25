3f07fa11d4d21c42b31b555f1399b376a356bbac
Brendan Tierney <tiernebre@gmail.com>
2024-02-25 11:28:20 +0000

    initialize command

--- .devcontainer/initializeCommand.sh
+++ .devcontainer/initializeCommand.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ -z "${CI}" ]; then
+  echo "Certification does not need to be created as this is being ran in a CI environment. Skipping and proceeding with dev container creation."
+  exit 0
+fi
+
 if [ -f .devcontainer/certs/localhost.cert ]; then
   echo "Certification was already created. Skipping certification creation and proceeding with dev container creation."
   exit 0
