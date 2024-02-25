9cb3968c32bbebe7a67b2a3778cff7c8081dfd75
Brendan Tierney <tiernebre@gmail.com>
2024-02-25 21:41:54 +0000

    health endpoint

--- src/main/java/com/tiernebre/web/RouterFactory.java
+++ src/main/java/com/tiernebre/web/RouterFactory.java
@@ -1,10 +1,11 @@
 package com.tiernebre.web;
 
 import com.tiernebre.web.controllers.FrontPageController;
+import com.tiernebre.web.controllers.HealthController;
 
 public final class RouterFactory {
 
   public Router create() {
-    return new Router(new FrontPageController());
+    return new Router(new FrontPageController(), new HealthController());
   }
 }
