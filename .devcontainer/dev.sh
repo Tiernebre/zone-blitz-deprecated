9f0e96bb823b27d4e6e0095b6e5b2fb7c81c8064
Brendan Tierney <tiernebre@gmail.com>
2024-02-27 16:54:55 +0000

    remove redundant ci

--- .devcontainer/dev.sh
+++ .devcontainer/dev.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-make install
 while true; do
   make serve &
+  PID=$!
+  echo "${PID}"
   inotifywait -r -e modify src/main/java/com/tiernebre src/main/resources/templates
+  gradle --stop
+  kill -9 "${PID}"
 done
