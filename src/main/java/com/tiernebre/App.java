e7899296d8602de407fb0bc45f745cf0b3a74f2a
Brendan Tierney <tiernebre@gmail.com>
2024-03-01 00:01:25 +0000

    WIP

--- src/main/java/com/tiernebre/App.java
+++ src/main/java/com/tiernebre/App.java
@@ -1,6 +1,13 @@
 package com.tiernebre;
 
+import com.tiernebre.jooq.tables.Player;
 import com.tiernebre.web.ServerFactory;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
 
 public class App {
 
