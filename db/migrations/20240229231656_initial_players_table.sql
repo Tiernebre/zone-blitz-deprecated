c1c3c65032c56cbfe42697ae0fa054130105b952
Brendan Tierney <tiernebre@gmail.com>
2024-03-01 02:45:12 -0700

    Setup jOOQ Code Generation that works with DBMate migrations (#74)

---
+++ db/migrations/20240229231656_initial_players_table.sql
@@ -0,0 +1,9 @@
+-- migrate:up
+CREATE TABLE IF NOT EXISTS player (
+  id BIGSERIAL PRIMARY KEY NOT NULL
+);
+
+
+-- migrate:down
+DROP TABLE IF EXISTS player;
+
--- src/main/java/com/tiernebre/App.java
+++ src/main/java/com/tiernebre/App.java
@@ -1,10 +1,32 @@
 package com.tiernebre;
 
+import com.tiernebre.jooq.tables.Player;
+import com.tiernebre.jooq.tables.records.PlayerRecord;
 import com.tiernebre.web.ServerFactory;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
 
 public class App {
 
   public static void main(String[] args) {
     new ServerFactory().create().start();
+    try {
+      var connection = DriverManager.getConnection(
+        System.getenv("ZONE_BLITZ_POSTGRES_JDBC_URL"),
+        System.getenv("ZONE_BLITZ_POSTGRES_USER"),
+        System.getenv("ZONE_BLITZ_POSTGRES_PASSWORD")
+      );
+      var create = DSL.using(connection, SQLDialect.POSTGRES);
+      var result = create
+        .select()
+        .from(Player.PLAYER)
+        .fetchInto(PlayerRecord.class);
+      System.out.println(result.size());
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 }
