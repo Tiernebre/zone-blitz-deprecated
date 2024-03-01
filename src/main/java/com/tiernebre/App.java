626d45c5799ef642d0dcc64fd32097f302c57859
Brendan Tierney <tiernebre@gmail.com>
2024-03-01 00:22:53 +0000

    fix driver

--- src/main/java/com/tiernebre/App.java
+++ src/main/java/com/tiernebre/App.java
@@ -1,17 +1,22 @@
 package com.tiernebre;
 
-import com.tiernebre.jooq.tables.Player;
 import com.tiernebre.web.ServerFactory;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import org.jooq.Record;
-import org.jooq.Result;
-import org.jooq.SQLDialect;
-import org.jooq.impl.DSL;
 
 public class App {
 
   public static void main(String[] args) {
     new ServerFactory().create().start();
+    try {
+      var connection = DriverManager.getConnection(
+        System.getenv("ZONE_BLITZ_POSTGRES_JDBC_URL"),
+        System.getenv("ZONE_BLITZ_POSTGRES_USER"),
+        System.getenv("ZONE_BLITZ_POSTGRES_PASSWORD")
+      );
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 }
