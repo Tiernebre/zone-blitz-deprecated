293af5695b4be96418b5df5bb93ad3394085dcb6
Brendan Tierney <tiernebre@gmail.com>
2024-03-02 21:33:36 +0000

    result test

--- src/test/java/com/tiernebre/database/DatabaseConnectionFactoryTest.java
+++ src/test/java/com/tiernebre/database/DatabaseConnectionFactoryTest.java
@@ -1,5 +1,6 @@
 package com.tiernebre.database;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 import java.sql.SQLException;
@@ -11,5 +12,7 @@ public final class DatabaseConnectionFactoryTest {
   public void createsAWorkingConnection() throws SQLException {
     var connection = new DatabaseConnectionFactory().create();
     assertFalse(connection.isClosed());
+    var result = connection.createStatement().executeQuery("SELECT 1 as test");
+    assertEquals(1, result.findColumn("test"));
   }
 }
