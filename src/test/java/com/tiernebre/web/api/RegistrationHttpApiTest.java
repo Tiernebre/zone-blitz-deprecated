c8523705f754f22b98e9e8322fe20cab9030b81c
Brendan Tierney <tiernebre@gmail.com>
2024-03-07 19:20:21 +0000

    unique setup

--- src/test/java/com/tiernebre/web/api/RegistrationHttpApiTest.java
+++ src/test/java/com/tiernebre/web/api/RegistrationHttpApiTest.java
@@ -9,12 +9,30 @@ import java.io.IOException;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.Map;
+import java.util.UUID;
 import org.apache.http.entity.ContentType;
 import org.junit.Test;
 
 public class RegistrationHttpApiTest {
 
   Javalin server = WebHttpTestUtils.startServer();
+
+  @Test
+  public void POST_registration_empty_body()
+    throws IOException, InterruptedException {
+    var response = WebHttpTestUtils.client()
+      .send(
+        HttpRequest.newBuilder(WebHttpTestUtils.url("/api/registration"))
+          .header(
+            "Content-Type",
+            ContentType.APPLICATION_FORM_URLENCODED.toString()
+          )
+          .POST(UrlEncodedFormBodyPublisher.ofParams(Map.of()))
+          .build(),
+        BodyHandlers.ofLines()
+      );
+    assertEquals(400, response.statusCode());
+  }
 
   @Test
   public void POST_registration() throws IOException, InterruptedException {
@@ -27,7 +45,12 @@ public class RegistrationHttpApiTest {
           )
           .POST(
             UrlEncodedFormBodyPublisher.ofParams(
-              Map.of("username", "username", "password", "password")
+              Map.of(
+                "username",
+                UUID.randomUUID().toString(),
+                "password",
+                "password"
+              )
             )
           )
           .build(),
