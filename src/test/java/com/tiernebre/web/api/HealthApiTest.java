2da31e86dacd25a7f891eaeba39d5012c7feaa07
Brendan Tierney <tiernebre@gmail.com>
2024-03-07 18:25:16 +0000

    better api testing infra

--- src/test/java/com/tiernebre/web/api/HealthApiTest.java
+++ src/test/java/com/tiernebre/web/api/HealthApiTest.java
@@ -2,18 +2,26 @@ package com.tiernebre.web.api;
 
 import static org.junit.Assert.assertEquals;
 
-import com.tiernebre.web.TestServer;
-import io.javalin.testtools.JavalinTest;
+import com.tiernebre.web.WebHttpTestUtils;
+import io.javalin.Javalin;
+import java.io.IOException;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
 import org.junit.Test;
 
 public class HealthApiTest {
 
+  Javalin server = WebHttpTestUtils.startServer();
+
   @Test
-  public void GET_healthy() {
-    JavalinTest.test(TestServer.create(), (server, client) -> {
-      var response = client.get("/api/health");
-      assertEquals(200, response.code());
-      assertEquals("Healthy", response.body().string());
-    });
+  public void GET_healthy() throws IOException, InterruptedException {
+    var response = WebHttpTestUtils.client()
+      .send(
+        HttpRequest.newBuilder(WebHttpTestUtils.url("/api/health"))
+          .GET()
+          .build(),
+        BodyHandlers.ofLines()
+      );
+    assertEquals(200, response.statusCode());
   }
 }
