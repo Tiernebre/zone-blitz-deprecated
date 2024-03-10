6d34bd2ea50fc67fc56e861fd5d51fcdd9760e97
Brendan Tierney <tiernebre@gmail.com>
2024-03-10 14:38:48 +0000

    ts formatting

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,45 +1,47 @@
-import { PlaywrightTestConfig, defineConfig, devices } from '@playwright/test';
+import { PlaywrightTestConfig, defineConfig, devices } from "@playwright/test";
 
-const URL = process.env.ZONE_BLITZ_URL || 'http://0.0.0.0:8000'
+const URL = process.env.ZONE_BLITZ_URL || "http://0.0.0.0:8000";
 
 const LOCAL_CONFIG: PlaywrightTestConfig = {
   use: {
     headless: true,
     channel: "/usr/bin/chromium-browser",
     launchOptions: {
-      executablePath: "/usr/bin/chromium-browser"
-    }
+      executablePath: "/usr/bin/chromium-browser",
+    },
   },
   webServer: {
-    command: 'make',
+    command: "make",
     url: URL,
-  }
-}
+  },
+};
 
 const CI_CONFIG: PlaywrightTestConfig = {
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
     },
 
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
     },
-  ]
-}
+  ],
+};
 
-const additionalConfiguration: PlaywrightTestConfig = process.env.CI ? CI_CONFIG : LOCAL_CONFIG;
+const additionalConfiguration: PlaywrightTestConfig = process.env.CI
+  ? CI_CONFIG
+  : LOCAL_CONFIG;
 
 export default defineConfig({
   ...additionalConfiguration,
-  testDir: 'e2e',
+  testDir: "e2e",
   use: {
     baseURL: URL,
     ...additionalConfiguration.use,
--- uno.config.ts
+++ uno.config.ts
@@ -1,18 +1,13 @@
-import { defineConfig } from 'unocss'
-import presetUno from '@unocss/preset-uno'
-import { presetForms } from '@julr/unocss-preset-forms'
+import { defineConfig } from "unocss";
+import presetUno from "@unocss/preset-uno";
+import { presetForms } from "@julr/unocss-preset-forms";
 
 export default defineConfig({
   content: {
-    filesystem: [
-      'src/main/resources/templates/**/*.mustache'
-    ],
+    filesystem: ["src/main/resources/templates/**/*.mustache"],
     pipeline: {
-      include: ['src/main/resources/templates/**/*.mustache']
-    }
+      include: ["src/main/resources/templates/**/*.mustache"],
+    },
   },
-  presets: [
-    presetUno(),
-    presetForms(),
-  ],
-})
+  presets: [presetUno(), presetForms()],
+});
