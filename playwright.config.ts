9a29772f31b38322b6e5247cefee3a826cbbe2a0
Brendan Tierney <tiernebre@gmail.com>
2024-03-10 15:19:28 +0000

    only use chromium

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,16 +22,6 @@ const CI_CONFIG: PlaywrightTestConfig = {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
   ],
 };
 
