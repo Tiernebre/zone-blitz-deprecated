1176740f42e37c8a8022ab4cadb0c982b0199d1c
Brendan Tierney <tiernebre@gmail.com>
2024-03-10 14:17:26 +0000

    playwright setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: 'e2e',
   use: {
-    baseURL: 'https://dev.zoneblitz.app',
+    baseURL: 'http://0.0.0.0:8000',
     headless: true,
     channel: "/usr/bin/chromium-browser",
     launchOptions: {
       executablePath: "/usr/bin/chromium-browser"
     }
+  },
+  webServer: {
+    command: 'make',
+    url: 'http://0.0.0.0:8000',
   }
 });
