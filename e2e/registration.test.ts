aca67044fb8744749ef652a4e660bbbde406abbe
Brendan Tierney <tiernebre@gmail.com>
2024-03-10 16:25:41 +0000

    requires a username

--- e2e/registration.test.ts
+++ e2e/registration.test.ts
@@ -1,17 +1,39 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, Page } from "@playwright/test";
 import crypto from "node:crypto";
 
-test("registers a user", async ({ page }) => {
-  const registrationsPage = await page.goto("/registration");
+const URI = "/registration";
+const PASSWORD = crypto.randomUUID();
+
+const getUsernameInput = (page: Page) =>
+  page.getByRole("textbox", { name: /Username/i });
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(URI);
+});
+
+test("registration page exists", async ({ page }) => {
+  const registrationsPage = await page.goto(URI);
   expect(registrationsPage).not.toBeNull();
   expect(registrationsPage!.status()).toStrictEqual(200);
-  const password = crypto.randomUUID();
-  await page
-    .getByRole("textbox", { name: /Username/i })
-    .fill(crypto.randomUUID());
-  await page.getByLabel("Password", { exact: true }).fill(password);
-  await page.getByLabel("Confirm Password", { exact: true }).fill(password);
+});
+
+test("registers a user", async ({ page }) => {
+  await getUsernameInput(page).fill(crypto.randomUUID());
+  await page.getByLabel("Password", { exact: true }).fill(PASSWORD);
+  await page.getByLabel("Confirm Password", { exact: true }).fill(PASSWORD);
   await page.getByRole("button", { name: /Register/i }).click();
   await page.waitForURL("/");
   expect(page.url()).not.toContain("registration");
+});
+
+test("requires a username", async ({ page }) => {
+  const password = crypto.randomUUID();
+  await page.getByLabel("Password", { exact: true }).fill(password);
+  await page.getByLabel("Confirm Password", { exact: true }).fill(password);
+  await page.getByRole("button", { name: /Register/i }).click();
+  expect(
+    await getUsernameInput(page).evaluate((node: HTMLInputElement) =>
+      node.matches(":invalid"),
+    ),
+  ).toBeTruthy();
 });
