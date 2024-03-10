8aee88454a689269686ee47eca5523a2c0c64638
Brendan Tierney <tiernebre@gmail.com>
2024-03-10 14:38:24 +0000

    config for CI and local

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from '@playwright/test';
+import { PlaywrightTestConfig, defineConfig, devices } from '@playwright/test';
 
 const URL = process.env.ZONE_BLITZ_URL || 'http://0.0.0.0:8000'
 
-export default defineConfig({
-  testDir: 'e2e',
+const LOCAL_CONFIG: PlaywrightTestConfig = {
   use: {
-    baseURL: URL,
     headless: true,
     channel: "/usr/bin/chromium-browser",
     launchOptions: {
@@ -16,4 +14,34 @@ export default defineConfig({
     command: 'make',
     url: URL,
   }
+}
+
+const CI_CONFIG: PlaywrightTestConfig = {
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ]
+}
+
+const additionalConfiguration: PlaywrightTestConfig = process.env.CI ? CI_CONFIG : LOCAL_CONFIG;
+
+export default defineConfig({
+  ...additionalConfiguration,
+  testDir: 'e2e',
+  use: {
+    baseURL: URL,
+    ...additionalConfiguration.use,
+  },
 });
