120afd6fbcf1bbe18889197f87c3122ab829a0e9
Brendan Tierney <tiernebre@gmail.com>
2024-03-15 13:12:52 +0000

    killed

--- .devcontainer/dev.sh
+++ .devcontainer/dev.sh
@@ -3,11 +3,11 @@
 set -e
 
 while true; do
+  trap 'kill -9 $(pgrep -f "java -classpath")' SIGINT SIGTERM EXIT
   rm -fr build
   make clean
   make install
   make start &
-  trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
   inotifywait -r -e modify src/main/java/com/tiernebre src/main/resources/templates
   kill -9 $(pgrep -f "java -classpath")
 done
