b3026191768d05bfc6a6eed8bb3f1cdadab0eb5c
Brendan Tierney <tiernebre@gmail.com>
2024-03-16 18:22:32 +0000

    initial script

--- .devcontainer/addProxyHost.sh
+++ .devcontainer/addProxyHost.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-echo "Adding proxy host to /etc/hosts"
-DOMAIN=$(dig +short proxy)
-echo "Adding $DOMAIN"
+DOMAIN=proxy
+IP=$(dig +short $DOMAIN)
+HOST_ENTRY="$IP $DOMAIN"
+
+if grep -q "$DOMAIN" /etc/hosts; then
+  echo "${DOMAIN} already exists in /etc/hosts. Skipping."
+else
+  echo "Adding $HOST_ENTRY to /etc/hosts"
+  echo "${HOST_ENTRY}" | tee -a /etc/hosts > /dev/null
+  echo "Added ${HOST_ENTRY} to /etc/hosts"
+fi
