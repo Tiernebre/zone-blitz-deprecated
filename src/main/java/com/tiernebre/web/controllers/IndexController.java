3d49d8ec8d47fd27d66d2d2800a8c61b9545c9ac
Brendan Tierney <tiernebre@gmail.com>
2024-03-16 17:07:38 +0000

    session is getting set

--- src/main/java/com/tiernebre/web/controllers/IndexController.java
+++ src/main/java/com/tiernebre/web/controllers/IndexController.java
@@ -1,9 +1,8 @@
 package com.tiernebre.web.controllers;
 
-import com.tiernebre.authentication.session.Session;
 import com.tiernebre.web.templates.Index;
 import io.javalin.http.Context;
-import io.jstach.jstachio.JStachio;
+import java.io.IOException;
 
 public final class IndexController {
 
@@ -13,18 +12,7 @@ public final class IndexController {
     this.helper = helper;
   }
 
-  public void render(Context ctx) {
-    var output = new StringBuilder();
-    JStachio.render(
-      new Index(
-        helper
-          .session(ctx)
-          .map(Session::accountId)
-          .map(Object::toString)
-          .getOrElse("null")
-      ),
-      output
-    );
-    ctx.html(output.toString());
+  public void render(Context ctx) throws IOException {
+    helper.template(ctx, new Index());
   }
 }
