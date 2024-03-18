c401f75ee0d88c69434f2149fdc5a1fd69ea0b7d
Brendan Tierney <tiernebre@gmail.com>
2024-03-18 15:09:42 +0000

    old migration

---
+++ db/migrations/20240318150916_go-back-to-text-type-for-hashed-passwords.sql
@@ -0,0 +1,10 @@
+-- migrate:up
+ALTER TABLE registration DROP COLUMN password;
+ALTER TABLE registration ADD COLUMN password TEXT NOT NULL;
+
+
+-- migrate:down
+ALTER TABLE registration DROP COLUMN password;
+ALTER TABLE registration ADD COLUMN password BYTEA NOT NULL;
+
+
