fbd570a1c9475b926cf6d6a2ee8ca8197b5cf893
Brendan Tierney <tiernebre@gmail.com>
2024-03-20 09:36:38 +0000

    Session middleware cleanup

--- src/main/java/com/tiernebre/web/middlewares/SessionMiddleware.java
+++ src/main/java/com/tiernebre/web/middlewares/SessionMiddleware.java
@@ -5,7 +5,6 @@ import com.tiernebre.web.constants.WebConstants;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 import io.vavr.control.Option;
-import io.vavr.control.Try;
 import java.util.UUID;
 import org.jetbrains.annotations.NotNull;
 
@@ -20,7 +19,9 @@ public final class SessionMiddleware implements Handler {
   @Override
   public void handle(@NotNull Context ctx) throws Exception {
     Option.of(ctx.cookie(WebConstants.SESSION_COOKIE_TOKEN_NAME))
-      .flatMap(token -> Try.of(() -> UUID.fromString(token)).toOption())
+      .toTry()
+      .mapTry(token -> UUID.fromString(token))
+      .toOption()
       .flatMap(sessionService::get)
       .peek(session -> {
         ctx.attribute(WebConstants.JAVALIN_SESSION_ATTRIBUTE, session);
