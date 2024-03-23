9dfef0b10a612c86014b0a130f30aa2c0aa44862
Brendan Tierney <tiernebre@gmail.com>
2024-03-23 17:22:24 +0000

    empty and null string handling for cursor

--- src/main/java/com/tiernebre/util/pagination/Base64CursorMapper.java
+++ src/main/java/com/tiernebre/util/pagination/Base64CursorMapper.java
@@ -1,7 +1,9 @@
 package com.tiernebre.util.pagination;
 
+import io.vavr.control.Option;
 import java.util.Base64.Decoder;
 import java.util.Base64.Encoder;
+import org.apache.commons.lang3.StringUtils;
 
 public final class Base64CursorMapper implements CursorMapper {
 
@@ -20,6 +22,12 @@ public final class Base64CursorMapper implements CursorMapper {
 
   @Override
   public long cursorToId(String cursor) {
-    return Long.parseLong(new String(decoder.decode(cursor)), 2);
+    return Option.of(cursor)
+      .filter(StringUtils::isNotBlank)
+      .map(
+        validCursor ->
+          Long.parseLong(new String(decoder.decode(validCursor)), 2)
+      )
+      .getOrElse(0L);
   }
 }
--- src/test/java/com/tiernebre/util/pagination/Base64CursorMapperTest.java
+++ src/test/java/com/tiernebre/util/pagination/Base64CursorMapperTest.java
@@ -3,6 +3,9 @@ package com.tiernebre.util.pagination;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import com.tiernebre.test.TestCase;
+import com.tiernebre.test.TestCaseRunner;
+import io.vavr.collection.List;
 import java.util.Base64;
 import org.junit.jupiter.api.Test;
 
@@ -20,5 +23,18 @@ public final class Base64CursorMapperTest {
     assertNotEquals(Long.toString(id), cursor);
     long mappedBack = base64CursorMapper.cursorToId(cursor);
     assertEquals(id, mappedBack);
+  }
+
+  @Test
+  public void mapsEmptyStrings() {
+    TestCaseRunner.run(
+      Base64CursorMapperTest.class,
+      List.of(
+        new TestCase<String, Long>("null", null, __ -> 0L),
+        new TestCase<String, Long>("empty", "", __ -> 0L),
+        new TestCase<String, Long>("blank", " ", __ -> 0L)
+      ),
+      base64CursorMapper::cursorToId
+    );
   }
 }
