e014660eaa936f5734fae59c488d5fa0f51b233b
Brendan Tierney <tiernebre@gmail.com>
2024-03-26 08:48:37 +0000

    create validation

--- src/main/java/com/tiernebre/league_management/league/LeagueService.java
+++ src/main/java/com/tiernebre/league_management/league/LeagueService.java
@@ -1,10 +1,15 @@
 package com.tiernebre.league_management.league;
 
+import com.tiernebre.util.error.ZoneBlitzError;
 import com.tiernebre.util.pagination.Page;
 import com.tiernebre.util.pagination.PageRequest;
+import io.vavr.control.Either;
 
 public interface LeagueService {
-  public League create(long accountId, UserLeagueRequest request);
+  public Either<ZoneBlitzError, League> create(
+    long accountId,
+    UserLeagueRequest request
+  );
 
   public Page<League> pageForAccount(long accountId, PageRequest request);
 }
