6d83349296afed328c20e36f4f96407ab4b77b89
Brendan Tierney <tiernebre@gmail.com>
2024-03-28 08:28:21 +0000

    error page cleanup

--- src/main/java/com/tiernebre/web/errors/NotFoundHandler.java
+++ src/main/java/com/tiernebre/web/errors/NotFoundHandler.java
@@ -1,27 +1,32 @@
 package com.tiernebre.web.errors;
 
+import com.tiernebre.web.util.WebHelper;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 import io.javalin.http.HttpStatus;
-import io.jstach.jstachio.JStachio;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class NotFoundHandler implements Handler {
 
+  private final WebHelper helper;
+
   private static final Logger LOG = LoggerFactory.getLogger(
     NotFoundHandler.class
   );
 
+  public NotFoundHandler(WebHelper helper) {
+    this.helper = helper;
+  }
+
   @Override
   public void handle(@NotNull Context ctx) throws Exception {
     LOG.debug("Requested route {} was not found.", ctx.path());
-    ctx.html(
-      JStachio.render(
-        new com.tiernebre.web.templates.Error(
-          "The requested page could not be found."
-        )
+    helper.template(
+      ctx,
+      new com.tiernebre.web.templates.Error(
+        "The requested page could not be found."
       )
     );
     ctx.status(HttpStatus.NOT_FOUND);
