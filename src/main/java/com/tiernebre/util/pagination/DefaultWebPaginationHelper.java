d63e3643c5251cf614fe956075b438c96742b585
Brendan Tierney <tiernebre@gmail.com>
2024-03-29 21:56:18 +0000

    pagination mapping tests

--- src/main/java/com/tiernebre/util/pagination/DefaultWebPaginationHelper.java
+++ src/main/java/com/tiernebre/util/pagination/DefaultWebPaginationHelper.java
@@ -7,13 +7,20 @@ public final class DefaultWebPaginationHelper implements WebPaginationHelper {
 
   @Override
   public PageRequest pageRequest(Context ctx) {
-    return new PageRequest(
-      Option.of(ctx.queryParam(PaginationConstants.FIRST_QUERY_PARAM_NAME))
-        .toTry()
-        .mapTry(Integer::parseInt)
-        .toOption()
-        .getOrElse(PaginationConstants.DEFAULT_PAGE_SIZE),
-      ctx.queryParam(PaginationConstants.CURSOR_QUERY_PARAM_NAME)
-    );
+    return Option.of(ctx)
+      .map(
+        context ->
+          new PageRequest(
+            Option.of(
+              context.queryParam(PaginationConstants.FIRST_QUERY_PARAM_NAME)
+            )
+              .toTry()
+              .mapTry(Integer::parseInt)
+              .toOption()
+              .getOrElse(PaginationConstants.DEFAULT_PAGE_SIZE),
+            context.queryParam(PaginationConstants.CURSOR_QUERY_PARAM_NAME)
+          )
+      )
+      .getOrElse(PaginationConstants.DEFAULT_PAGE_REQUEST);
   }
 }
--- src/test/java/com/tiernebre/util/pagination/DefaultPaginationWebHelperTest.java
+++ src/test/java/com/tiernebre/util/pagination/DefaultPaginationWebHelperTest.java
@@ -1,6 +1,7 @@
 package com.tiernebre.util.pagination;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import com.tiernebre.test.TestCase;
 import com.tiernebre.test.TestCaseRunner;
@@ -18,9 +19,63 @@ public final class DefaultPaginationWebHelperTest {
       DefaultPaginationWebHelperTest.class,
       List.of(
         new TestCase<Context, PageRequest>(
+          "null context",
+          null,
+          __ -> PaginationConstants.DEFAULT_PAGE_REQUEST
+        ),
+        new TestCase<Context, PageRequest>(
           "null first",
           mock(Context.class),
-          __ -> new PageRequest(PaginationConstants.DEFAULT_PAGE_SIZE, null)
+          __ -> PaginationConstants.DEFAULT_PAGE_REQUEST,
+          (input, output) -> {
+            when(
+              input.queryParam(PaginationConstants.FIRST_QUERY_PARAM_NAME)
+            ).thenReturn(null);
+          }
+        ),
+        new TestCase<Context, PageRequest>(
+          "null cursor",
+          mock(Context.class),
+          __ -> PaginationConstants.DEFAULT_PAGE_REQUEST,
+          (input, output) -> {
+            when(
+              input.queryParam(PaginationConstants.CURSOR_QUERY_PARAM_NAME)
+            ).thenReturn(null);
+          }
+        ),
+        new TestCase<Context, PageRequest>(
+          "custom first",
+          mock(Context.class),
+          __ -> new PageRequest(302, null),
+          (input, output) -> {
+            when(
+              input.queryParam(PaginationConstants.FIRST_QUERY_PARAM_NAME)
+            ).thenReturn(Integer.toString(output.first()));
+          }
+        ),
+        new TestCase<Context, PageRequest>(
+          "custom cursor",
+          mock(Context.class),
+          __ ->
+            new PageRequest(PaginationConstants.DEFAULT_PAGE_SIZE, "abcdef"),
+          (input, output) -> {
+            when(
+              input.queryParam(PaginationConstants.FIRST_QUERY_PARAM_NAME)
+            ).thenReturn(null);
+            when(
+              input.queryParam(PaginationConstants.CURSOR_QUERY_PARAM_NAME)
+            ).thenReturn(output.after());
+          }
+        ),
+        new TestCase<Context, PageRequest>(
+          "non number size",
+          mock(Context.class),
+          __ -> PaginationConstants.DEFAULT_PAGE_REQUEST,
+          (input, output) -> {
+            when(
+              input.queryParam(PaginationConstants.FIRST_QUERY_PARAM_NAME)
+            ).thenReturn("not a number");
+          }
         )
       ),
       webPaginationHelper::pageRequest
