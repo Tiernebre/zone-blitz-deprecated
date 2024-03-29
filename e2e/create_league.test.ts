2fae9f1031a2ee95850f53349665006177ecd1e5
Brendan Tierney <tiernebre@gmail.com>
2024-03-29 22:15:54 +0000

    create league setup

--- e2e/create_league.test.ts
+++ e2e/create_league.test.ts
@@ -3,20 +3,23 @@ import { register } from "./helpers";
 import { VALIDATION_MESSAGES, expect } from "./expect";
 import crypto from "node:crypto";
 
+const URI = "/leagues/create";
+
 const getNameInput = (page: Page) =>
   page.getByRole("textbox", { name: /name/i });
 const submit = (page: Page) =>
   page.getByRole("button", { name: /create/i }).click();
 
-test("renders for a logged in user", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await register(page);
-  await page.goto("/leagues/create");
+  await page.goto(URI);
+});
+
+test("renders for a logged in user", async ({ page }) => {
   await expect(page.getByText(/create league/i)).toBeVisible();
 });
 
 test("creates a league", async ({ page }) => {
-  await register(page);
-  await page.goto("/leagues/create");
   const name = `LEAGUE-${crypto.randomUUID().toString()}`;
   await getNameInput(page).fill(name);
   await submit(page);
@@ -26,17 +29,13 @@ test("creates a league", async ({ page }) => {
 });
 
 test("enforces maximum length on league name", async ({ page }) => {
-  await register(page);
-  await page.goto("/leagues/create");
   const name = "a".repeat(64);
   await getNameInput(page).fill(name + "a");
   await expect(getNameInput(page)).toHaveValue(name);
 });
 
 test("enforces that league name is required", async ({ page }) => {
-  await register(page);
-  await page.goto("/leagues/create");
   await submit(page);
   await expect(getNameInput(page)).toBeInvalid(VALIDATION_MESSAGES.REQUIRED);
-  await expect(page).toHaveURL("/leagues/create");
+  await expect(page).toHaveURL(URI);
 });
