e32eb9739ce8265319e7d839edd3ce5aea3ed0f0
Brendan Tierney <tiernebre@gmail.com>
2024-03-29 10:02:42 +0000

    encapsulation cleanup

--- src/main/java/com/tiernebre/web/controllers/authentication/AuthenticationWebConstants.java
+++ src/main/java/com/tiernebre/web/controllers/authentication/AuthenticationWebConstants.java
@@ -18,12 +18,13 @@ public final class AuthenticationWebConstants {
       AuthenticationConstants.PASSWORD_MAXIMUM_LENGTH,
       AuthenticationConstants.PASSWORD_MINIMUM_LENGTH
     );
-  static final String GOOGLE_ACCOUNTS_URL = "https://accounts.google.com";
+  public static final String GOOGLE_ACCOUNTS_URL =
+    "https://accounts.google.com";
 
-  static final String GOOGLE_CLIENT_ID = System.getenv(
+  public static final String GOOGLE_CLIENT_ID = System.getenv(
     "ZONE_BLITZ_OAUTH_GOOGLE_CLIENT_ID"
   );
-  static final String GOOGLE_CREDENTIAL_FIELD_NAME = "credential";
+  public static final String GOOGLE_CREDENTIAL_FIELD_NAME = "credential";
   public static final String GOOGLE_CSRF_TOKEN_FIELD_NAME = "g_csrf_token";
   public static final String GOOGLE_STATE_FIELD_NAME = "g_state";
   public static final GoogleSignOnButtonConfiguration GOOGLE_SIGN_ON_BUTTON_CONFIGURATION =
