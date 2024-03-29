2687d9982cfe8b7a779d0f3ba172612168912073
Brendan Tierney <tiernebre@gmail.com>
2024-03-29 21:23:21 +0000

    max length in template

--- src/main/java/com/tiernebre/league_management/league/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.tiernebre.league_management.league;
-
-final class Constants {
-
-  static int NAME_MAXIMUM_LENGTH = 64;
-}
--- src/main/java/com/tiernebre/web/templates/CreateLeague.java
+++ src/main/java/com/tiernebre/web/templates/CreateLeague.java
@@ -1,5 +1,6 @@
 package com.tiernebre.web.templates;
 
+import com.tiernebre.league_management.league.LeagueManagementConstants;
 import com.tiernebre.web.controllers.league_management.LeagueManagementWebConstants;
 import com.tiernebre.web.templates.interfaces.UsesForm;
 import io.jstach.jstache.JStache;
@@ -8,5 +9,9 @@ import io.jstach.jstache.JStache;
 public record CreateLeague(String error) implements UsesForm {
   String nameFieldName() {
     return LeagueManagementWebConstants.LEAGUE_NAME_FIELD_NAME;
+  }
+
+  int nameMaximumLength() {
+    return LeagueManagementConstants.NAME_MAXIMUM_LENGTH;
   }
 }
