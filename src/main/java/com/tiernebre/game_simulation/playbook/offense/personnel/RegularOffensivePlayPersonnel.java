8c705196d7d29746841f475bf20a02137930fcd4
Brendan Tierney <tiernebre@gmail.com>
2024-06-16 23:35:49 +0000

    abstract class approach instead

--- src/main/java/com/tiernebre/game_simulation/playbook/offense/personnel/RegularOffensivePlayPersonnel.java
+++ src/main/java/com/tiernebre/game_simulation/playbook/offense/personnel/RegularOffensivePlayPersonnel.java
@@ -1,5 +1,6 @@
 package com.tiernebre.game_simulation.playbook.offense.personnel;
 
+import com.tiernebre.game_simulation.EngineConstants;
 import com.tiernebre.game_simulation.dto.Player;
 import com.tiernebre.game_simulation.dto.personnel.OffensiveLine;
 
@@ -23,5 +24,39 @@ public abstract class RegularOffensivePlayPersonnel {
     this.tightEnds = tightEnds;
     this.wideReceivers = wideReceivers;
     this.offensiveLine = offensiveLine;
+  }
+
+  public Player[] players() {
+    Player[] players =
+      new Player[EngineConstants.NUMBER_OF_PLAYERS_ON_FIELD_PER_SIDE];
+    var playersIndex = 0;
+    for (int i = 0; i < runningBacks.length; i++) {
+      players[playersIndex++] = runningBacks[i];
+    }
+    for (int i = 0; i < wideReceivers.length; i++) {
+      players[playersIndex++] = wideReceivers[i];
+    }
+    for (int i = 0; i < tightEnds.length; i++) {
+      players[playersIndex++] = tightEnds[i];
+    }
+    players[playersIndex++] = quarterback;
+    players[playersIndex++] = offensiveLine.leftTackle();
+    players[playersIndex++] = offensiveLine.leftGuard();
+    players[playersIndex++] = offensiveLine.center();
+    players[playersIndex++] = offensiveLine.rightGuard();
+    players[playersIndex++] = offensiveLine.rightTackle();
+    return players;
+  }
+
+  public Player[] offensiveLinemen() {
+    Player[] offensiveLinemen =
+      new Player[EngineConstants.NUMBER_OF_OFFENSIVE_LINEMEN];
+    var i = 0;
+    offensiveLinemen[i++] = offensiveLine.leftTackle();
+    offensiveLinemen[i++] = offensiveLine.leftGuard();
+    offensiveLinemen[i++] = offensiveLine.center();
+    offensiveLinemen[i++] = offensiveLine.rightGuard();
+    offensiveLinemen[i++] = offensiveLine.rightTackle();
+    return offensiveLinemen;
   }
 }
