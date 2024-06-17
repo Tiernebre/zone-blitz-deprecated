2243812997571d99db3299f4789ce13d77da71ef
Brendan Tierney <tiernebre@gmail.com>
2024-06-17 00:12:03 +0000

    moved defensive personnel over to abstraction in tests

---
+++ src/main/java/com/tiernebre/game_simulation/playbook/defense/FourThreeDefensivePersonnel.java
@@ -0,0 +1,17 @@
+package com.tiernebre.game_simulation.playbook.defense;
+
+import com.tiernebre.game_simulation.dto.Player;
+
+public class FourThreeDefensivePersonnel extends RegularPlayDefensivePersonnel {
+
+  public FourThreeDefensivePersonnel(
+    Player[] linemen,
+    Player[] linebackers,
+    Player[] cornerbacks,
+    Player freeSafety,
+    Player strongSafety
+  ) {
+    super(linemen, linebackers, cornerbacks, freeSafety, strongSafety);
+    //TODO Auto-generated constructor stub
+  }
+}
