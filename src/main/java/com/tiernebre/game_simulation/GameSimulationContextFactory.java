2c164e901de8bbb707c65aca51318cf57bc6e800
Brendan Tierney <tiernebre@gmail.com>
2024-07-05 06:02:41 +0000

    game simulation

--- src/main/java/com/tiernebre/game_simulation/GameSimulationContextFactory.java
+++ src/main/java/com/tiernebre/game_simulation/GameSimulationContextFactory.java
@@ -1,41 +1,14 @@
 package com.tiernebre.game_simulation;
 
-import com.tiernebre.game_simulation.dto.game.ScoringConfiguration;
 import com.tiernebre.game_simulation.game.DefaultGameSimulator;
-import com.tiernebre.game_simulation.game.state.DefaultGameStateMachine;
-import com.tiernebre.game_simulation.game.state.handlers.field_goal.DefaultFieldGoalHandler;
-import com.tiernebre.game_simulation.game.state.handlers.punt.DefaultPuntHandler;
-import com.tiernebre.game_simulation.game.state.handlers.regular.DefaultRegularPlayHandler;
-import com.tiernebre.game_simulation.game.state.handlers.scoring.DefaultScoringPlayHandler;
-import com.tiernebre.game_simulation.game.state.handlers.turnover.DefaultTurnoverHandler;
+import com.tiernebre.game_simulation.game.state.GameStateMachineFactory;
 
-public class GameSimulationContextFactory {
+public final class GameSimulationContextFactory {
 
   public GameSimulationContext create() {
-    var scoringConfiguration = new ScoringConfiguration(7, 3, 1, 2, 0);
-    var scoringPlayHandler = new DefaultScoringPlayHandler(
-      scoringConfiguration
+    return new GameSimulationContext(
+      // TODO: inject a play simulator
+      new DefaultGameSimulator(null, new GameStateMachineFactory().create())
     );
-    var turnoverHandler = new DefaultTurnoverHandler();
-    var regularPlayHandler = new DefaultRegularPlayHandler(
-      scoringPlayHandler,
-      turnoverHandler
-    );
-    var fieldGoalHandler = new DefaultFieldGoalHandler(
-      turnoverHandler,
-      scoringPlayHandler
-    );
-    var puntHandler = new DefaultPuntHandler();
-    var gameStateMachine = new DefaultGameStateMachine(
-      regularPlayHandler,
-      fieldGoalHandler,
-      puntHandler,
-      // TODO: Implement kickoff handler
-      null
-    );
-
-    // TODO: inject a play simulator
-    var gameSimulator = new DefaultGameSimulator(null, gameStateMachine);
-    return new GameSimulationContext(gameSimulator);
   }
 }
