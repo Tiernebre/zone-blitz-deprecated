72d29d63b32050054c29724a0478e95694074fcf
Brendan Tierney <tiernebre@gmail.com>
2024-07-05 17:38:06 +0000

    initial stub

---
+++ src/main/java/com/tiernebre/game_simulation/game/play/kickoff/DefaultKickoffSimulator.java
@@ -0,0 +1,13 @@
+package com.tiernebre.game_simulation.game.play.kickoff;
+
+public class DefaultKickoffSimulator implements KickoffSimulator {
+
+  @Override
+  public KickoffResult simulate(
+    KickReturnPlayCall kickReturnPlayCall,
+    KickoffPlayCall kickoffPlayCall
+  ) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'simulate'");
+  }
+}
