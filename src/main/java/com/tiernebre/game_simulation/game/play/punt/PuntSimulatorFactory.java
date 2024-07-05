2c4917c4f9f402c4585df5ce3469fd4d0973590e
Brendan Tierney <tiernebre@gmail.com>
2024-07-05 11:37:08 -0600

    Punt Simulator Factory (#223)

---
+++ src/main/java/com/tiernebre/game_simulation/game/play/punt/PuntSimulatorFactory.java
@@ -0,0 +1,14 @@
+package com.tiernebre.game_simulation.game.play.punt;
+
+import org.apache.commons.math3.distribution.NormalDistribution;
+
+public final class PuntSimulatorFactory {
+
+  public PuntSimulator create() {
+    return new DefaultPuntSimulator(
+      new DistributionPuntDistanceCalculator(
+        new NormalDistribution(43.9941, 9.444654)
+      )
+    );
+  }
+}
